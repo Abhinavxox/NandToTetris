9ddfffc90d9d3b65cd5e7a55428df367be89c5b6
Abhinavxox <72064600+Abhinavxox@users.noreply.github.com>
2022-07-01 13:35:25 +0530

    <completed>:[Library for tokenizer created]

--- Compiler/Library.java
+++ Compiler/Library.java
@@ -1,10 +1,51 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class Library{
 
     public static String token(String token){
-        if(Character.isDigit(token.charAt(0))){
+
+        //for keyword
+        ArrayList<String> keyword = new ArrayList<>(Arrays.asList(
+        "class",    
+        "constructor",
+        "do",
+        "else",
+        "false",
+        "field",
+        "function",
+        "if",
+        "let",
+        "method",
+        "null",
+        "return",
+        "static",
+        "this",
+        "true",
+        "var",
+        "while"));
+
+        //if it is a digit
+        if(token.matches("[0-9]+")){
             return "integerConstant"; 
         }
-        
-    }
+        //if it is a symbol
+        else if(token.matches("[\\&\\*\\+\\(\\)\\.\\/\\,\\-\\]\\;\\~\\}\\|\\{\\>\\=\\[\\<]")){
+            return "symbol"; 
+        }
+        //if it is a string
+        else if(token.matches("\"[^\"\n]*\"")){
+            return "stringConstant";
+        }
+        //if it is a keyword 
+        else if(keyword.contains(token)){
+            return "keyword";
+        }
+        //if it is an identifier
+        else if(token.matches("[\\w_]+")){
+            return "identifier";
+        }
+        return null;
+    }   
 
 }
