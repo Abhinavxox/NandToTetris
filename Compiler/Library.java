224adbce113fed358ed1cade7839e4999813dd9c
Abhinavxox <72064600+Abhinavxox@users.noreply.github.com>
2022-07-01 11:31:44 +0530

    <Compiler>:[Intialised]

---
+++ Compiler/Library.java
@@ -0,0 +1,10 @@
+class Library{
+
+    public static String token(String token){
+        if(Character.isDigit(token.charAt(0))){
+            return "integerConstant"; 
+        }
+        
+    }
+
+}
